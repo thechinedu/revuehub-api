300cb541fab7157239ada50082e166c5283d7742
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-08-30 09:46:48 +0200

    Add functionality for creating user via oauth [status: incomplete, sn: 001] (#64)

---
+++ src/users/dto/create-oauth-user-dto.ts
@@ -0,0 +1,4 @@
+export type CreateOauthUserDto = {
+  code: string;
+  state: string;
+};
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateOauthStateDto } from './dto/create-oauth-state-dto';
+import fetch from 'node-fetch';
 
+import { CreateOauthStateDto } from './dto/create-oauth-state-dto';
+import { CreateOauthUserDto } from './dto/create-oauth-user-dto';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UserModel } from './user.model';
+
+const AUTH_ENDPOINT = process.env.GITHUB_AUTH_ENDPOINT;
+const OAUTH_CLIENT_ID = process.env.GITHUB_OAUTH_CLIENT_ID;
+const OAUTH_CLIENT_SECRET = process.env.GITHUB_OAUTH_CLIENT_SECRET;
 
 @Injectable()
 export class UserService {
@@ -14,5 +20,33 @@ export class UserService {
 
   createOauthState(createOauthStateDto: CreateOauthStateDto) {
     return this.userModel.createOauthState(createOauthStateDto);
+  }
+
+  async createOauthUser(createOauthUserDto: CreateOauthUserDto) {
+    // oauthProvider = new OAuthProvider(provider)
+    // accessToken = oauthProvider.exchangeCodeForAccessToken(code)
+    // if accessToken -> authorized successfully
+    // else -> an error occured. return failure response
+    // oauthProvider.getUserInfo(accessToken) -> { email: string, username: string }
+    // oauthProvider.getAccessToken()*
+
+    // Find oauth provider using the state value
+    // Send a post request to oauth provider to exchange the code for an access token
+    // With access token present, make a request to fetch the user's information
+    // Save user information along with oauth access token
+
+    const res = await fetch(
+      `${process.env.GITHUB_AUTH_ENDPOINT}?client_id=${OAUTH_CLIENT_ID}&client_secret=${OAUTH_CLIENT_SECRET}&code=${createOauthUserDto.code}`,
+      {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+        },
+      },
+    );
+    const json = await res.json();
+    const accessToken = json.access_token;
+
+    return {};
   }
 }
