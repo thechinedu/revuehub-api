88d3d8937d79979cc82398fafbdf473f5c3dd0a7
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-09-12 21:34:46 +0200

    Add login functionality [status: incomplete, sn: 002]

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './Auth.service';
 // import { UserModel } from './user.model';
 
 @Module({
+  imports: [JwtModule.register({ secret: process.env.APP_SECRET as string })],
   controllers: [AuthController],
   providers: [AuthService],
 })
