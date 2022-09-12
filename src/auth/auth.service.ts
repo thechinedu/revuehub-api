2fef393536b3a39c7e479384c9b70be2c159b989
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-09-12 21:35:25 +0200

    Add login functionality [status: incomplete, sn: 002] (#80)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { randomBytes } from 'crypto';
 import { UserCredentialsDto } from './dto/user-credentials-dto';
 
 @Injectable()
 export class AuthService {
-  loginUser(userCredentialsDto: UserCredentialsDto) {}
+  constructor(private jwtService: JwtService) {}
+
+  loginUser(userCredentialsDto: UserCredentialsDto) {
+    // const accessToken = this.jwtService.sign({
+    //   email: userCredentialsDto.email,
+    // });
+    // const refreshToken = randomBytes(32).toString('base64');
+    //
+    // Get user via email address
+    // Generate accessToken using jwt service (expires in 15 minutes)
+    // Generate unique refresh token (check db for existence of generated token and regenerate if it exists)
+    //    expires in 48 hours
+    // Insert refresh token into user_auth_tokens table
+    // Send 204 response to client
+    // Store accessToken and refreshToken pair as secure httpOnly cookies
+    //
+  }
 }
