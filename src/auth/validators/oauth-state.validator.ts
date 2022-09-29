b6c1915182c11c69c4ccf745ed90540b4c44afad
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-09-29 10:50:20 +0200

    Add OAuth state functionality (#107)
    * Add validation for oauth/state endpoint
    
    * Update createOAuthState and fetchOAuthUserInfo methods

---
+++ src/auth/validators/oauth-state.validator.ts
@@ -0,0 +1,56 @@
+import { OAuthProviders } from '@/types';
+import Joi, { CustomValidator, ValidationErrorItem } from 'joi';
+
+import { UserCredentialsDto } from '../dto/user-credentials-dto';
+
+const { object, string } = Joi.types();
+
+const validateProviderValue: CustomValidator<string> = (value, helpers) => {
+  const supportedProviders = Object.values(OAuthProviders) as string[];
+
+  if (!supportedProviders.includes(value)) {
+    return helpers.error('any.invalid');
+  }
+
+  return value;
+};
+
+export const schema: Joi.ObjectSchema<UserCredentialsDto> = object.keys({
+  provider: string.required().custom(validateProviderValue),
+});
+
+const providerValidationMessages = {
+  'string.empty': { password: 'Provier cannot be empty' },
+  'any.required': { provider: 'No provider provided' },
+  'string.base': { provider: 'Provider must be a string' },
+  'any.invalid': {
+    provider: 'Provider is not supported',
+  },
+};
+
+export const oauthStateValidator = {
+  schema,
+  // TODO: Make this a reusable utility
+  serializeValidationMessages: (errorDetails: ValidationErrorItem[]) => {
+    const actions = {
+      provider: providerValidationMessages,
+    };
+    const acc = {};
+
+    errorDetails.forEach((details) => {
+      const { context, type } = details;
+      let messageRecord: Record<string, string> | ValidationErrorItem;
+
+      if (context?.key) {
+        const key = actions[context.key as keyof typeof actions];
+        messageRecord = key?.[type as keyof typeof key];
+      } else {
+        messageRecord = details;
+      }
+
+      Object.assign(acc, messageRecord);
+    });
+
+    return { data: acc };
+  },
+};
--- utils/oauth/generate-oauth-state.ts
+++ utils/oauth/generate-oauth-state.ts
@@ -1,3 +1,13 @@
-export const generateOAuthState = () => {
-  // noop
+import { memoryStore } from '@/db';
+import { generateRandomToken } from '../auth';
+
+export const generateOAuthState = async (): Promise<string> => {
+  const memoryStoreClient = await memoryStore;
+  const state = generateRandomToken(32);
+
+  const keyExists = Boolean(await memoryStoreClient.exists(state));
+
+  if (keyExists) return generateOAuthState();
+
+  return state;
 };
