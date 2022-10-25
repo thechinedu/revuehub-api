b0a5a985e7731b86365677284f9179d24b77fea4
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-10-25 09:18:31 +0200

    Add AuthGuard support [status: incomplete, sn: 001] (#146)

--- src/repositories/repositories.module.ts
+++ src/repositories/repositories.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { RepositoriesController } from './repositories.controller';
 import { RepositoryModel } from './repository.model';
 import { RepositoryService } from './repository.service';
 
 @Module({
+  imports: [JwtModule.register({ secret: process.env.APP_SECRET as string })],
   controllers: [RepositoriesController],
   providers: [RepositoryModel, RepositoryService],
 })
