4a340232af3bdba70dbe3967ade345126d93a109
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-10-25 09:11:36 +0200

    Add AuthGuard support [status: incomplete, sn: 001]

--- src/guards/auth.ts
+++ src/guards/auth.ts
@@ -1,10 +1,32 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest();
-    console.log({ cookie: req.cookies['refreshToken'] });
-    return true;
+
+    try {
+      const payload = this.jwtService.verify(req.cookies.accessToken);
+      console.log({ payload });
+
+      return true;
+    } catch (err) {
+      throw new HttpException(
+        {
+          status: 'fail',
+          message: 'You are not authorized to access this resource',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
   }
 }
