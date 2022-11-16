1d161f18ad8754d44be6f2064bc442c0d74da66f
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-11-16 10:03:15 +0100

    Add support for fetching the contents of a repository [status: incomplete, sn: 001] (#168)

--- migrations/20221116085227_create_repository_blobs.ts
+++ migrations/20221116085227_create_repository_blobs.ts
@@ -1,10 +1,9 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('repository_contents', (table) => {
+  return knex.schema.createTable('repository_blobs', (table) => {
     table.increments();
     table.integer('repository_id').unsigned();
-    table.string('type');
     table.timestamps(false, true);
 
     table.foreign('repository_id').references('repositories.id');
@@ -12,5 +11,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('repository_contents');
+  return knex.schema.dropTable('repository_blobs');
 }
