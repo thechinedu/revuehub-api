3244c05e292be581b97bdc1f28022371d9decf97
Chinedu Daniel <chinedudaniel7@gmail.com>
2022-12-14 08:54:58 +0100

    Add support for fetching repo tree and blob contents [status: incomplete, sn: 003]

--- seeds/create-repositories.ts
+++ seeds/create-repositories.ts
@@ -1,13 +1,31 @@
 import { faker } from '@faker-js/faker';
 import { Knex } from 'knex';
 
-function createRepository() {
+// table.increments();
+// table.string('email', 255).unique();
+// table.string('username', 255).unique();
+// table.string('password_digest', 128);
+// table.boolean('email_verified').defaultTo(false);
+// table.string('provider', 255).defaultTo(''); // TODO: Move to a separate table. A user can potentially have multiple oauth providers (only github supported for now but other providers might be supported in the future (gitlab, google, twitter etc))
+// table.string('full_name', 255).defaultTo('');
+// table.string('profile_image_url', 255);
+// table.timestamps(false, true);
+function createUser() {
   return {
-    snapshot_id: faker.datatype.uuid(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    full_name: faker.name.fullName(),
+  };
+}
+
+function createRepository(user_id: number) {
+  return {
+    user_id,
+    snapshot_id: faker.datatype.number(),
     node_id: faker.datatype.uuid(),
-    user_id: faker.datatype.number(),
     name: `thechinedu/revuehub-api`,
     description: faker.company.catchPhrase(),
+    default_branch: 'main',
     last_updated: faker.date.past(),
     last_synced: faker.date.recent(),
     has_pulled_content: false,
@@ -15,9 +33,10 @@ function createRepository() {
 }
 
 export async function seed(knex: Knex): Promise<void> {
-  // Deletes ALL existing entries
+  await knex('users').del();
   await knex('repositories').del();
 
-  // Inserts seed entries
-  await knex('repositories').insert(createRepository());
+  const user = (await knex('users').insert(createUser()).returning(['id']))[0];
+
+  await knex('repositories').insert(createRepository(user.id));
 }
