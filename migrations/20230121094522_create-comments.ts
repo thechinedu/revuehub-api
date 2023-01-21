5f652401217ac74f9dc9ec8861710c8d0a8fe2c6
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-01-21 10:59:34 +0100

    Add comments table

---
+++ migrations/20230121094522_create-comments.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('comments', (table) => {
+    table.increments();
+    table.integer('user_id').unsigned();
+    table.integer('repository_blob_id').unsigned();
+    table.integer('parent_comment_id').unsigned().nullable();
+    table.text('content').notNullable();
+    table.integer('start_line');
+    table.integer('end_line');
+    table
+      .enum('status', ['PENDING', 'PUBLISHED', 'RESOLVED'])
+      .defaultTo('PENDING');
+    table.timestamps(false, true);
+
+    table.foreign('user_id').references('users.id');
+    table.foreign('repository_blob_id').references('repository_blobs.id');
+    table.foreign('parent_comment_id').references('comments.id');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('comments');
+}
