dba6e610112a5b4abd5c107251f6817a83a06874
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-01-31 21:48:05 +0100

    Add additional validation message handlers to create-comment validator

--- src/comments/validators/create-comment.validator.ts
+++ src/comments/validators/create-comment.validator.ts
@@ -15,6 +15,18 @@ export const isSupportedValue: (values: string[]) => CustomValidator<string> =
       return helpers.error('any.invalid');
     }
 
+    if (value === CommentLevel.LINE) {
+      const {
+        state: { ancestors },
+      } = helpers;
+      const { start_line: startLine, end_line: endLine } = ancestors?.[0];
+
+      if (startLine == null || endLine == null)
+        return helpers.error('any.missing-line');
+
+      if (endLine < startLine) return helpers.error('any.invalid-lines');
+    }
+
     return value;
   };
 
@@ -25,25 +37,37 @@ const validateCommentLevel = (): CustomValidator<string> =>
   isSupportedValue(Object.values(CommentLevel));
 
 const schema: Joi.ObjectSchema<CreateCommentDto> = object.keys({
-  user_id: number.required().greater(0),
   repository_blob_id: number.required().greater(0),
   parent_comment_id: number.optional().greater(0),
   content: string.required(),
-  start_line: number.optional(),
-  end_line: number.optional(),
   status: string.optional().custom(validateCommentStatus()),
   level: string.required().custom(validateCommentLevel()),
+  start_line: number.optional().greater(0),
+  end_line: number.optional().greater(0),
 });
 
 const messages = {
-  user_id: generateValidationMessages({ field: 'user_id' }),
   repository_blob_id: generateValidationMessages({
     field: 'repository_blob_id',
   }),
   parent_comment_id: generateValidationMessages({ field: 'parent_comment_id' }),
   content: generateValidationMessages({ field: 'content', label: 'Content' }),
-  start_line: generateValidationMessages({ field: 'start_line' }),
-  end_line: generateValidationMessages({ field: 'end_line' }),
+  start_line: generateValidationMessages({
+    field: 'start_line',
+    overrides: {
+      'number.greater': {
+        start_line: 'start_line must be a value greater than 0',
+      },
+    },
+  }),
+  end_line: generateValidationMessages({
+    field: 'end_line',
+    overrides: {
+      'number.greater': {
+        end_line: 'end_line must be a value greater than 0',
+      },
+    },
+  }),
   status: generateValidationMessages({
     field: 'status',
     label: 'Status',
@@ -63,6 +87,13 @@ const messages = {
         level: `Level is not valid. Level must be one of [${Object.values(
           CommentLevel,
         )}]`,
+      },
+      'any.missing-line': {
+        level:
+          "start_line and end_line must be provided when the comment level is set to 'LINE'",
+      },
+      'any.invalid-lines': {
+        level: 'start_line must be less than end_line',
       },
     },
   }),
