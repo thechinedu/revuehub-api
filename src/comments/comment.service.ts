d97ea51fdf45702879d016f71d67c5e990001375
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-02-16 11:27:21 +0100

    Add support for submitting review comments [status: incomplete, sn: 001] (#236)

--- src/comments/comment.service.ts
+++ src/comments/comment.service.ts
@@ -1,3 +1,4 @@
+import { CommentLevel, CommentStatus } from '@/types';
 import { Injectable } from '@nestjs/common';
 import { CommentModel } from './comment.model';
 
@@ -8,6 +9,13 @@ export class CommentService {
   constructor(private commentModel: CommentModel) {}
 
   createComment(createCommentDto: CreateCommentDto) {
+    if (createCommentDto.level === CommentLevel.PROJECT) {
+      createCommentDto.status = CommentStatus.PUBLISHED;
+    }
+
     this.commentModel.create(createCommentDto);
+    /* if the project-level comment is saved successfully, it means that the user has successfully submitted a review
+     * schedule updates to happen to pending comments that are a part of the review
+     */
   }
 }
