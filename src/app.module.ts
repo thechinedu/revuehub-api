327d41d59329cd2be7682ed1bcb6d94067d24d05
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-02-20 09:18:31 +0100

    Add support for submitting review comments [status: incomplete, sn: 002] (#242)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { AuthModule } from '@/src/auth/auth.module';
 import { CommentsModule } from '@/src/comments/comments.module';
 import { RepositoriesModule } from '@/src/repositories/repositories.module';
 import { UsersModule } from '@/src/users/users.module';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule, CommentsModule, RepositoriesModule, UsersModule],
+  imports: [
+    BullModule.forRoot({
+      redis: process.env.REDIS_URL,
+    }),
+    AuthModule,
+    CommentsModule,
+    RepositoriesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/comments/comment.model.ts
+++ src/comments/comment.model.ts
@@ -1,11 +1,55 @@
 import { db } from '@/db';
+import { CommentLevel, CommentStatus } from '@/types';
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 import { CreateCommentDto } from './dto/create-comment-dto';
 
+type CommentEntity = {
+  id: number;
+  user_id: number;
+  repository_blob_id: number;
+  repository_content_id: number;
+  repository_id: number;
+  parent_comment_id?: number;
+  publish_id?: number;
+  content: string;
+  start_line?: number;
+  end_line?: number;
+  insertion_pos?: number;
+  status: CommentStatus;
+  level: CommentLevel;
+  created_at: Date;
+  updated_at: Date;
+};
+
+type CommentEntityKeys = keyof CommentEntity;
+
 @Injectable()
 export class CommentModel {
-  async create(createCommentDto: CreateCommentDto): Promise<void> {
-    await db('comments').insert(createCommentDto);
+  constructor(@InjectQueue('comments') private readonly commentQueue: Queue) {}
+
+  async create(createCommentDto: CreateCommentDto): Promise<CommentEntity> {
+    const rows = await db('comments').insert(createCommentDto).returning('*');
+    const res = rows[0] as CommentEntity;
+
+    if (res.level === CommentLevel.PROJECT) {
+      await this.commentQueue.add({
+        user_id: res.user_id,
+        publish_id: res.id,
+      });
+    }
+
+    return res;
   }
 }
+
+// ES import, export
+// default
+//  export default
+// import <>
+
+// NodeJS, commonJS
+// require, exports
+// module.export =
