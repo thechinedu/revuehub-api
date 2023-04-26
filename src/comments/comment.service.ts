2fec4fbd9124db1fedeb42b93eaff48d6d13ae9d
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-04-26 10:17:28 +0200

    Add support for retrieving review comments [status: incomplete, sn: 004]

--- src/comments/comment.service.ts
+++ src/comments/comment.service.ts
@@ -64,7 +64,7 @@ export class CommentService {
           },
           orderBy: [{ column: 'insertion_pos', order: 'asc' }],
           select: [
-            'user_id',
+            'content',
             'username',
             'profile_image_url',
             'status',
